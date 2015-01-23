c19118673bf4d5a7448c6f0f955e86c37f269c0e
Dmitry Dorofeev <me@dmitrydorofeev.ru>
2015-01-23 09:36:27 +0300

    optimize connection fix3

--- api/common.py
+++ api/common.py
@@ -1,12 +1,12 @@
 def list_following(cursor, user_id):
-    cursor.execute("""SELECT followee FROM follower WHERE follower = %s;""", user_id)
+    cursor.execute("""SELECT followee FROM follower WHERE follower = %s;""", (user_id, ))
     following = [i['email'] for i in cursor.fetchall()]
 
     return following
 
 
 def list_followers(cursor, user_id):
-    cursor.execute("""SELECT follower FROM follower WHERE followee = %s;""", user_id)
+    cursor.execute("""SELECT follower FROM follower WHERE followee = %s;""", (user_id, ))
     followers = [i['email'] for i in cursor.fetchall()]
 
     return followers
